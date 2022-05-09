57da2edf61f57b8f88f2810d7d8bf3225a4cec78
Wayne Kenney <wayneleokenney@gmail.com>
2022-05-09 19:32:37 -0400

    birch

--- vectorclusters/en_core_web_lg_vectors/make_birch.py
+++ vectorclusters/en_core_web_lg_vectors/make_birch.py
@@ -1,5 +1,4 @@
 from sklearn.cluster import Birch, OPTICS
-from sklearn.preprocessing import Normalizer
 import pandas as pd
 
 import time
@@ -9,10 +8,9 @@ import os
 logging.basicConfig(filename='make_birch.log',level=logging.INFO,filemode='w')
 logging.info(f"PID: {os.getpid()}")
 logging.info(f"Fitting Started {time.strftime('%m-%d-%H', time.localtime())}")
+os.makedirs('./birch/data',exist_ok=True)
 
-X = pd.read_parquet('./data/vectors.parquet').sample(n=10000)
-X = Normalizer().fit_transform(X=X)
-logging.info("Vectors Normalized")
+X = pd.read_parquet('./data/vectors.parquet')
 
 ap = OPTICS(min_samples=1000,metric='euclidean')
 bir = Birch(n_clusters=ap)
@@ -24,10 +22,8 @@ logging.info(f"Real Hours to fit: {(t2 - t1)/60/60}")
 
 clust_pred = bir.predict(X)
 t3 = time.time()
-logging.info(f"Real Hours to predict: {(t2-t3)/60/60}")
+logging.info(f"Real Hours to predict: {(t3-t2)/60/60}")
 logging.info(f"Cluster count: {len(set(clust_pred))}")
-
-os.makedirs('./birch/data/clusters.parquet',exist_ok=True)
 
 pd.DataFrame(clust_pred,columns=['cluster']).to_parquet('./birch/data/clusters.parquet')
 logging.info("Complete")
