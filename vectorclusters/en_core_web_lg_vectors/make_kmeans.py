980ec0b4d89a11d7837627ebee1fb77242eb97eb
Wayne Kenney <wayneleokenney@gmail.com>
2022-05-11 23:19:00 -0400

    kmeans

--- vectorclusters/en_core_web_lg_vectors/make_kmeans.py
+++ vectorclusters/en_core_web_lg_vectors/make_kmeans.py
@@ -11,20 +11,20 @@ logging.basicConfig(filename='make_kmeans.log',filemode='w',level=logging.INFO,
 
 X = dd.read_parquet('./data/vectors.parquet')
 
-
 n_samples = 200
 k_samples = np.random.randint(low=2,high=5000,size=n_samples)
 k_ineritas = zarr.open_array('./kmeans/data/k_ineritas.zarr', mode='w',
                     shape=(n_samples,2),chunks=None, fill_value=0,
                     dtype=float)
 
+print('Start')
 for index, k in enumerate(k_samples):
     t1 = time.time()
     km = KMeans(n_clusters=k,n_jobs=-1)
-    X = km.fit_transform(X)
-    k_ineritas[index,:] = (k,X.inertia_)
+    km.fit(X)
+    k_ineritas[index,:] = (k,km.inertia_)
 
     t2 = time.time()
-    logging.info(f"index: {index} finished. Hours: {(t2-t1)/60/60}")
+    print(f"index: {index} finished. Hours: {(t2-t1)/60/60}")
 
-logging.info(f"Complete")
+print(f"Complete")
