c972ab20ebf2b258ca2fcd40915028187d32cd69
Wayne Kenney <wayneleokenney@gmail.com>
2022-05-11 23:22:50 -0400

    kmeans

--- vectorclusters/en_core_web_lg_vectors/make_kmeans.py
+++ vectorclusters/en_core_web_lg_vectors/make_kmeans.py
@@ -17,7 +17,7 @@ k_ineritas = zarr.open_array('./kmeans/data/k_ineritas.zarr', mode='w',
                     shape=(n_samples,2),chunks=None, fill_value=0,
                     dtype=float)
 
-print('Start')
+print('Start',flush=True)
 for index, k in enumerate(k_samples):
     t1 = time.time()
     km = KMeans(n_clusters=k,n_jobs=-1)
@@ -25,6 +25,6 @@ for index, k in enumerate(k_samples):
     k_ineritas[index,:] = (k,km.inertia_)
 
     t2 = time.time()
-    print(f"index: {index} finished. Hours: {(t2-t1)/60/60}")
+    print(f"index: {index} finished. Hours: {(t2-t1)/60/60}",flush=True)
 
 print(f"Complete")
