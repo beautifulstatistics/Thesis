f81e7f000f95a7f32ba034525aaeee0539f04ced
Wayne Kenney <wayneleokenney@gmail.com>
2022-05-27 13:35:24 -0400

    randomize make_kmeans

--- vectors/en_core_web_lg_vectors/make_kmeans.py
+++ vectors/en_core_web_lg_vectors/make_kmeans.py
@@ -1,6 +1,6 @@
 from dask_ml.cluster import KMeans
 import dask.dataframe as dd
-import numpy as np
+import random
 import zarr
 
 import logging
@@ -9,30 +9,28 @@ import time
 logging.basicConfig(filename='make_kmeans.log',filemode='a',level=logging.INFO,
                     format="%(process)s-%(asctime)s-%(message)s")
 
+k_max = 200
+
 X = dd.read_parquet('./data/vectors.parquet')
 
-n_samples = 200
-k_samples = []
-
-while len(k_samples) < n_samples:
-    candidate = np.random.randint(low=2,high=2000,size=1)[0]
-    if candidate not in k_samples:
-        k_samples.append(candidate)
+clusters = list(range(2,k_max+1))
+random.shuffle(clusters)
+print(len(clusters))
 
 k_inertias = zarr.open_array('./kmeans/data/k_inertias.zarr', mode='w',
-                    shape=(n_samples,2),chunks=None, fill_value=0,
+                    shape=(len(clusters),2),chunks=None, fill_value=0,
                     dtype=float)
 
 print('Start',flush=True)
 t_start = time.time()
-for index, k in enumerate(k_samples):
+for index, k in enumerate(clusters):
     t1 = time.time()
     km = KMeans(n_clusters=k,n_jobs=-1)
     km.fit(X)
     k_inertias[index,:] = (k,km.inertia_)
 
-    time.sleep(60*30)
     t2 = time.time()
-    print(f"index: {index} finished. Hours: {(t2-t1)/60/60}, Time Left: {(t2-t_start)/(index+1)*(n_samples-index+1)}",flush=True)
+    print(f"Index: {index} finished, Clusters: {k}, Hours: {round((t2-t1)/60/60,2)}, " \
+          f"Days Left: {round((t2-t_start)/(index+1)*(len(clusters)-index-1)/60/60/24,2)}",flush=True)
 
 print(f"Complete")
