e3a1feefebd9bf7f7c090ebf50f421d329b8e64d
Wayne Kenney <wayneleokenney@gmail.com>
2022-06-02 18:45:15 -0400

    saving update before change

--- vectors/en_core_web_lg_vectors/make_kmeans_update.py
+++ vectors/en_core_web_lg_vectors/make_kmeans_update.py
@@ -2,28 +2,35 @@ from dask_ml.cluster import KMeans
 import dask.dataframe as dd
 import random
 import zarr
+import numpy as np
 
 import logging
 import time
 
-logging.basicConfig(filename='make_kmeans.log',filemode='a',level=logging.INFO,
+logging.basicConfig(filename='./logs/make_kmeans5.log',filemode='a',level=logging.INFO,
                     format="%(process)s-%(asctime)s-%(message)s")
 
-k_max = 300
+k_max = 500
 
 X = dd.read_parquet('./data/vectors.parquet')
 
 clusters = list(range(2,k_max+1))
 random.shuffle(clusters)
-print(len(clusters))
 
-k_inertias = zarr.open_array('./kmeans/data/k_inertias.zarr', mode='w',
+k_inertiaso = zarr.open_array('./kmeans/data/k_inertias4.zarr', mode='r')
+k_inertias = zarr.open_array('./kmeans/data/k_inertias5.zarr', mode='w',
                     shape=(len(clusters),2),chunks=None, fill_value=0,
                     dtype=float)
 
 print('Start',flush=True)
 t_start = time.time()
 for index, k in enumerate(clusters):
+    ktf = k_inertiaso[:,0] == k
+    if any(ktf):
+        k_inertias[index,:] = k_inertiaso[np.where(ktf)[0][0]]
+        print(f"{k} found.",flush=True)
+        continue
+
     t1 = time.time()
     km = KMeans(n_clusters=k,n_jobs=-1)
     km.fit(X)
@@ -31,6 +38,6 @@ for index, k in enumerate(clusters):
 
     t2 = time.time()
     print(f"Index: {index} finished, Clusters: {k}, Hours: {round((t2-t1)/60/60,2)}, " \
-          f"Days Left: {round((t2-t_start)/(index+1)*(len(clusters)-index-1)/60/60/24,2)}",flush=True)
+          f"Hours Left: {round((t2-t_start)/(index+1)*(len(clusters)-index-1)/60/60,2)}",flush=True)
 
 print(f"Complete")
