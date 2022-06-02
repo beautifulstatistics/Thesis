f7085d6c9234630479ba2cb19500afaf69483b87
Wayne Kenney <wayneleokenney@gmail.com>
2022-06-02 18:48:50 -0400

    kmeans sample 15%

--- vectors/en_core_web_lg_vectors/make_kmeans_update.py
+++ vectors/en_core_web_lg_vectors/make_kmeans_update.py
@@ -1,40 +1,41 @@
 from dask_ml.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
 import dask.dataframe as dd
 import random
 import zarr
-import numpy as np
 
 import logging
 import time
 
-logging.basicConfig(filename='./logs/make_kmeans5.log',filemode='a',level=logging.INFO,
+logging.basicConfig(filename='./logs/make_kmeans6.log',filemode='a',level=logging.INFO,
                     format="%(process)s-%(asctime)s-%(message)s")
 
-k_max = 500
+k_max = 1000
+k_sam = 150
 
 X = dd.read_parquet('./data/vectors.parquet')
 
-clusters = list(range(2,k_max+1))
-random.shuffle(clusters)
+clusters = [1]
+while len(clusters) < k_sam:
+    randi = random.randint(a=2,b=k_max)
+    if randi not in clusters:
+        clusters.append(randi)
 
-k_inertiaso = zarr.open_array('./kmeans/data/k_inertias4.zarr', mode='r')
-k_inertias = zarr.open_array('./kmeans/data/k_inertias5.zarr', mode='w',
-                    shape=(len(clusters),2),chunks=None, fill_value=0,
+k_inertias_sam = zarr.open_array('./kmeans/data/k_inertias_sam.zarr', mode='w',
+                    shape=(len(clusters),3),chunks=None, fill_value=0,
                     dtype=float)
 
 print('Start',flush=True)
 t_start = time.time()
 for index, k in enumerate(clusters):
-    ktf = k_inertiaso[:,0] == k
-    if any(ktf):
-        k_inertias[index,:] = k_inertiaso[np.where(ktf)[0][0]]
-        print(f"{k} found.",flush=True)
-        continue
-
     t1 = time.time()
+    
     km = KMeans(n_clusters=k,n_jobs=-1)
     km.fit(X)
-    k_inertias[index,:] = (k,km.inertia_)
+
+    sil_score = silhouette_score(X=X,labels=km.labels_)
+    k_inertias_sam[index,:] = (k,km.inertia_,sil_score)
 
     t2 = time.time()
     print(f"Index: {index} finished, Clusters: {k}, Hours: {round((t2-t1)/60/60,2)}, " \
