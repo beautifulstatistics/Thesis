7a6e752de364de1082654b21cfb75f77c65fec25
Wayne Kenney <wayneleokenney@gmail.com>
2022-06-30 22:23:46 -0400

    updated make_kmeans_list

--- vectors/en_core_web_lg_vectors/make_kmeans_list.py
+++ vectors/en_core_web_lg_vectors/make_kmeans_list.py
@@ -6,9 +6,9 @@ import time
 
 X = pd.read_parquet('./data/vectors.parquet')
 
-clusters = list(range(45000,52500,500))
+clusters = list(range(15000,20000,1000))
 
-path = './kmeans/data/k_inertias_45000_52500.zarr'
+path = './kmeans/data/k_inertias_15000_20000.zarr'
 if not os.path.exists(path):
     k_inertias_sample = zarr.open_array(path, mode='w',
                     shape=(len(clusters),2),chunks=None, fill_value=0,
@@ -33,7 +33,8 @@ for index, k in enumerate(clusters):
     k_inertias_sample[index,:] = (k,km.inertia_)
 
     t2 = time.time()
-    print(f"Index: {index} finished, Clusters: {k}, Hours: {round((t2-t1)/60/60,2)}, " \
+    tt = time.strftime('%H:%M', time.localtime())
+    print(f"Index: {index} finished, Clusters: {k}, Time {tt} Hours: {round((t2-t1)/60/60,2)}, " \
           f"Hours Left: {round((t2-t_start)/(index+1)*(len(clusters)-index-1)/60/60,2)}",flush=True)
 
 print(f"Complete")
